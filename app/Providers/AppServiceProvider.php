9f1e359123aff40a4c74df67e89772cfd2c8f2a6
Faisalramdani <muhamadfaisalramdani724@gmail.com>
2025-04-20 13:30:18 +0700

    Menambahkan ULR Asssest menjadi HTTPS dan merubah APP_URL ENV

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,11 +20,13 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-
-
     public function boot()
     {
         if (app()->environment('production')) {
+            // Force all URLs to use HTTPS
+            URL::forceScheme('https');
+
+            // Migrate database automatically
             try {
                 Artisan::call('migrate', ['--force' => true]);
             } catch (\Exception $e) {
