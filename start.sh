87efb8ad982ba43ad6236b3e998fd83f8c91756c
Faisalramdani <muhamadfaisalramdani724@gmail.com>
2025-04-20 17:27:36 +0700

    menambahkan seeder di start.sh

--- start.sh
+++ start.sh
@@ -7,5 +7,8 @@ sleep 5
 # Jalankan migrate
 php artisan migrate:fresh --seed --force
 
+# Jalankan Seeder
+php artisan db:seed --force
+
 # Jalankan server
 php artisan serve --host=0.0.0.0 --port=${PORT}
